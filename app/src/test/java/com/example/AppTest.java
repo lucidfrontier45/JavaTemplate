4baef4427ccc8f82212d3f9130b3ccf2f80bb71f
Du Shiqiao <lucidfrontier.45@gmail.com>
2022-07-23 00:22:59 +0900

    add files

---
+++ app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+  @Test
+  void appHasGreeting() {
+    App classUnderTest = new App();
+    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+  }
+}
